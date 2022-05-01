7a3dc8f1c83816ce970d0acdbfc02b4327b55414
ad551 <gdaim551@gmail.com>
2022-05-01 12:22:10 +0500

    earliest commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+//fs.mkdirSync('practice');
+
+//fs.writeFileSync('practice/bio.txt',"My name is Abdul Daim");
+
+//fs.appendFileSync('practice/bio.txt'," BSSE");
+
+//const data = fs.readFileSync('practice/bio.txt',"utf8");
+
+//console.log(data);
+
+//fs.renameSync('practice/bio.txt', 'practice/test.txt');
+
+//fs.unlinkSync('practice/test.txt');
+
+fs.rmdirSync('practice');
